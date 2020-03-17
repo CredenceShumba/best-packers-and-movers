00b7849e1969e8c89accbcd435e5cbcb4f4491db
ZoltanPellei <58769757+ZoltanPellei@users.noreply.github.com>
2020-03-17 22:35:58 +0000

    Test commit Z.

--- best-packers-and-movers/src/main/java/com/nttdata/teambest/bestpackersandmovers/model/User.java
+++ best-packers-and-movers/src/main/java/com/nttdata/teambest/bestpackersandmovers/model/User.java
@@ -5,5 +5,20 @@ public class User {
 	private String userName;
 	//just a comment
 	private String userType;
+	//anoter comment
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	
+	
 
 }
