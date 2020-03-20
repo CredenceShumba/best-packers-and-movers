62dbe1cc076efa126763823122396fe544cc599d
CredenceShumba <credence.shumba@nttdata.com>
2020-03-20 15:10:59 +0000

    Swagger api documentation

--- best-packers-and-movers/src/main/java/com/nttdata/bestpackersandmovers/BestPackersAndMoversApplication.java
+++ best-packers-and-movers/src/main/java/com/nttdata/bestpackersandmovers/BestPackersAndMoversApplication.java
@@ -3,7 +3,10 @@ package com.nttdata.bestpackersandmovers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class BestPackersAndMoversApplication {
 
 	public static void main(String[] args) {
